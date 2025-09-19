f1deee606c8dfac8a54ffb2588cc39f16286250e
Chidiebere <chidiebereuzomahumble@gmail.com>
2025-09-19 14:25:40 +0100

    Added developer's name.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,10 +3,19 @@ import React from 'react'
 const Footer: React.FC = () => {
   return (
     <footer className="bg-white shadow mt-4">
-      <div className="container mx-auto text-center p-4">
-        <p className="text-gray-700">
+      <div className="container flex justify-center p-4 text-gray-700">
+        <p className="">
           &copy; {new Date().getFullYear()} Rueby's Cuisine. All rights
-          reserved.
+          reserved. &nbsp;
+        </p>
+        <p>
+          Developed by
+          <a
+            href="https://chidiebereuzoma.dev"
+            className="text-blue-500 hover:underline"
+          >
+            &nbsp; Chidiebere Uzoma
+          </a>
         </p>
       </div>
     </footer>
